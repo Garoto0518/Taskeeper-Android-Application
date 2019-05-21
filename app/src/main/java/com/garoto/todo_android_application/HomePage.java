902f0a38c6cf4f2fdea2ce78d5336783c968de4d
Gabriel Rodriguez - Torres <gabrielrguez0518@gmail.com>
2019-05-20 23:35:09 -0400

    User Interface started, Firebase next...

--- app/src/main/java/com/garoto/todo_android_application/HomePage.java
+++ app/src/main/java/com/garoto/todo_android_application/HomePage.java
@@ -1,13 +1,30 @@
 package com.garoto.todo_android_application;
 
+import android.graphics.Typeface;
+import android.media.MediaMetadata;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
+
+    TextView tituloDeAplicacion, subTituloDeAplicacion, endPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        tituloDeAplicacion = findViewById(R.id.tituloDeAplicacion);
+        subTituloDeAplicacion = findViewById(R.id.subTituloDeAplicacion);
+        endPage = findViewById(R.id.subTituloDeAplicacion);
+
+        //Font
+        Typeface mediumLight = Typeface.createFromAsset(getAssets(), "fonts/ML.ttf");
+        Typeface MMedium = Typeface.createFromAsset(getAssets(),"fonts/MM.ttf");
+
+        tituloDeAplicacion.setTypeface(MMedium);
+        subTituloDeAplicacion.setTypeface(mediumLight);
+        endPage.setTypeface(mediumLight);
     }
 }
